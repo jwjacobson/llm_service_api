23763f69d27b1da128917be2cc59ca70b1565732
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2025-04-29 10:28:56 -0400

    Add welcome view

--- llm_api/views.py
+++ llm_api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def welcome_view(request):
+    return Response({"message": "Welcome to the LLM API!"})
