a786737046378dd589ba230c58d30685392ad863
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2025-05-01 11:52:32 -0400

    Fix supported_models url

--- project/urls.py
+++ project/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', welcome_view, name='welcome'),
     path('api/signup/', RegisterView.as_view(), name='signup'),
-    path('supported-models/', SupportedModelsView.as_view(), name='supported-models'),
+    path('api/supported-models/', SupportedModelsView.as_view(), name='supported-models'),
 ]
