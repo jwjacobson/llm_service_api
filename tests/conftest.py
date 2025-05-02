4a469a1f560b723039a848147524ea9c69f930de
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2025-05-02 15:20:17 -0400

    Add docstrings to pytest fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,14 +5,17 @@ from rest_framework_simplejwt.tokens import RefreshToken
 
 @pytest.fixture
 def api_client():
+    """A basic client."""
     return APIClient()
 
 @pytest.fixture
 def user():
+    """A basic user."""
     return User.objects.create_user(username='user', password='password')
 
 @pytest.fixture
 def auth_client(user):
+    """A client with an authenticated user."""
     client = APIClient()
     refresh = RefreshToken.for_user(user)
     client.credentials(HTTP_AUTHORIZATION=f'Bearer {str(refresh.access_token)}')
