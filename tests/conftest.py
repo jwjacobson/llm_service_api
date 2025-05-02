36cd82f6f2cb5a1021441fa347924d5a145e207c
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2025-05-02 15:05:15 -0400

    Add missing import to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
-from rest_framework.test import APIClient
 from django.contrib.auth.models import User
+from rest_framework.test import APIClient
+from rest_framework_simplejwt.tokens import RefreshToken
 
 @pytest.fixture
 def api_client():
