8e73afecef8c418ac8720ed5f70b39509244a6c0
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2025-05-02 15:27:18 -0400

    Rename user fixture test_user

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,14 +9,14 @@ def api_client():
     return APIClient()
 
 @pytest.fixture
-def user():
+def test_user():
     """A basic user."""
     return User.objects.create_user(username='user', password='password')
 
 @pytest.fixture
-def auth_client(user):
+def auth_client(test_user):
     """A client with an authenticated user."""
     client = APIClient()
-    refresh = RefreshToken.for_user(user)
+    refresh = RefreshToken.for_user(test_user)
     client.credentials(HTTP_AUTHORIZATION=f'Bearer {str(refresh.access_token)}')
     return client
