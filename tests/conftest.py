59ff123989f1f597e7a4bc40f6648c1d41bb4515
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2025-05-02 15:04:17 -0400

    Add user and auth_client fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,18 @@
 import pytest
 from rest_framework.test import APIClient
+from django.contrib.auth.models import User
 
 @pytest.fixture
 def api_client():
     return APIClient()
+
+@pytest.fixture
+def user():
+    return User.objects.create_user(username='user', password='password')
+
+@pytest.fixture
+def auth_client(user):
+    client = APIClient()
+    refresh = RefreshToken.for_user(user)
+    client.credentials(HTTP_AUTHORIZATION=f'Bearer {str(refresh.access_token)}')
+    return client
