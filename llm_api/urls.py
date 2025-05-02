f88bee8aca9b8c9d633ea18ec74a61aaf1374bbc
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2025-05-02 12:11:58 -0400

    Create llm_api/urls.py, move app-specific urls there

---
+++ llm_api/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from llm_api.views import (
+    RegisterView,
+    welcome_view,
+    SupportedModelsView,
+    ChatCompletionsView
+)
+
+urlpatterns = [
+    path('', welcome_view, name='welcome'),
+    path('api/signup/', RegisterView.as_view(), name='signup'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/supported-models/', SupportedModelsView.as_view(), name='supported-models'),
+    path('api/chat/completions/', ChatCompletionsView.as_view(), name='chat-completions'),
+]
--- project/urls.py
+++ project/urls.py
@@ -15,23 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from django.urls import path, include
 
-from llm_api.views import (RegisterView,
-welcome_view,
-SupportedModelsView,
-ChatCompletionsView
-)
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('', welcome_view, name='welcome'),
-    path('api/signup/', RegisterView.as_view(), name='signup'),
-    path('api/supported-models/', SupportedModelsView.as_view(), name='supported-models'),
-    path('api/chat/completions/', ChatCompletionsView.as_view(), name='chat-completions'),
+    path("", include("llm_api.urls")),
 ]
